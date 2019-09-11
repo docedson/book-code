c7a37fe157114e32484de29379c6161a470a894b
Andrew Edson <C_Edson.Andrew@cerner.com>
2019-09-11 10:01:44 -0500

    1st change and push

--- 01-getting-started/01/spec/sandwich_spec.rb
+++ 01-getting-started/01/spec/sandwich_spec.rb
@@ -1,5 +1,8 @@
 
 RSpec.describe 'An ideal sandwich' do
   it 'is delicious' do
+    sandwich = Sandwich.new('delicious', [])
+    taste = sandwich.taste
+    expect(tatse).to eq('delicious')
   end
 end
